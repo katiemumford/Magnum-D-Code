7dd8ba19aa547abe3003607df5b833a012c19f64
Katie Mumford <katieamumford@gmail.com>
2020-02-27 21:47:13 -0800

    kind of working ish tray macro with manual control as well
    switch statement with three cases and working statements. won't work on robot but logic and almost all methods work

--- src/macroTrayControl.cpp
+++ src/macroTrayControl.cpp
@@ -1,18 +1,20 @@
 #include "vex.h"
 using namespace vex;
 
+
 const float voltConversion = 0.0944; 
-int trayState; //represents state of tray 0 = intake pos, 1 = defence pos, 2 = stack pos
+int trayState = 0; //represents state of tray 0 = intake pos, 1 = defence pos, 2 = stack pos
 
 //locations for positions 
-float defensePos = 40;     //CHANGE/TUNE
+float defensePos = -900;     //CHANGE/TUNE
 float stackCheckPoint1 = 30;
 float stackCheckPoint2 = 20;
-float stackCheckPoint3 = 10;
+float stackCheckPoint3 = -1700;
 
 
 //helper method for spinning the tray at a given voltage speed
 //takes trayPower and converts. so highest is 127 (12 in volts)
+//*note* negative number moves tray towards from limit switch, positive brings it away from limit switch 
 void moveTrayVolt(float trayPower){
   if (trayPower != 0){
     tray.spin(vex::directionType::fwd, double (trayPower * voltConversion), vex::voltageUnits::volt);
@@ -21,24 +23,20 @@ void moveTrayVolt(float trayPower){
   }
 }
 
-
-
-
 //moves tray to intake position and resets encoder
-void moveToIntakePos(){
-  if (trayLimit.value() != 1){           //if limit switch isn't hit (means tray isn't back yet) 
-    while (trayLimit.value() != 1){      //move tray back until limit switch IS hit
-      moveTrayVolt(100);
-    }
-  } else {
-    moveTrayVolt(0);                  //if limit switch is hit, means tray all the way back, so don't spin motor
-    tray.resetPosition();             //reset encoder of motor for greater accuracy for future use
-  }
+void moveToIntakePos(){                   //if limit switch isn't hit (means tray isn't back yet) 
+   if (trayLimit.value() != 1){      //move tray back until limit switch IS hit
+      moveTrayVolt(-60);
+      Controller.Screen.print("bruh");
+   } else {
+     moveTrayVolt(0);
+     tray.resetPosition();
+   }                      
 }
 
 //moves tray to defense pos
 void moveToDefensePos(){              
-  if (tray.position(deg) < defensePos){
+  if ((-1*tray.position(deg)) > defensePos){
     moveTrayVolt(50);
   } else {
     moveTrayVolt(0);
@@ -59,25 +57,7 @@ void moveToStackPos(){
 }
 
 
-void macroTrayControl(){
- switch(trayState){
-    case 0:
-      moveToIntakePos();
-      //Brain.Screen.printAt(10, 60, "base");  //apply continous down power eventually      
-      break;
-    case 1:
-       moveToDefensePos();
-       // Brain.Screen.printAt(10, 60, "score low twr");
-      break;
-    case 2:
-      moveToStackPos();
-       //Brain.Screen.printAt(10, 60, "score mid twr");
-      break;
-}
-}
-
-void testMe(){
-
+void manualTray(){
   if (Controller.ButtonL2.pressing()){    //if L2 continously pressed, move tray towards limit switch
     if (trayLimit.value()!=1){ //if limit switch isn't hit move tray
       moveTrayVolt(-70);
@@ -85,10 +65,59 @@ void testMe(){
       moveTrayVolt(0);
     }                     
   } else if (Controller.ButtonL1.pressing()){    //if L1 continously pressed, move tray away from limit switch
-    moveTrayVolt(20);
+    moveTrayVolt(70);
   } else{
     moveTrayVolt(0);
   }
+}
+
+
+void macroTrayControl(){
+ switch(trayState){
+    case 0:
+      moveToIntakePos();    
+      break;
+    case 1:
+      moveToDefensePos();
+      break;
+    case 2:
+      moveToStackPos();
+      break;
+    case 3:
+      manualTray();
+    break;
+}
+}
+
+
+void testMe(){
+
+  macroTrayControl();
+
+  if (aPressed()){   //get degrees for testing purposes
+    Controller.Screen.print(trayState);
+  }
+
+  if (xPressed()){
+    trayState = 0;
+  }
+
+  if (yPressed()){
+    trayState = 1;
+  }
+
+  if (bPressed()){
+    trayState = 2;
+  }
+
+  if (l1Pressed()){
+    Controller.Screen.print(tray.position(deg));
+  }
+
+  if(l2Pressed()){
+    trayState = 3;
+  }
+
   }
 
 
@@ -99,6 +128,7 @@ void testMe(){
 int allowMacroTrayForUser(){  //function that task calls
   while(true){
 
+    macroTrayControl();
     testMe();
 
     task::sleep(5);
